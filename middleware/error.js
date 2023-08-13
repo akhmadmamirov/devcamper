aa2ba6cf62ce12ae0b8bf1a4a0299b4a1a3ba7bc
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-08-13 14:44:06 -0400

    adding async middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,32 @@
-const errorHandler = (err, req, res, next) => {
-    //Log to the console for the dev
-    console.log(err.stack.red)
+const ErrorResponse = require("../utils/errorResponse");
 
-    res.status(err.statusCode || 500).json({success: false, error: err.message || 'Server Error'})
+const errorHandler = (err, req, res, next) => {
+    let error = {...err}
+
+    error.message = err.message
+
+    //Log to the console for the dev
+    console.log(err)
+
+    //Mongoos bad objectID
+    if (err.name === 'CastError'){
+        const message = "Resource not found";
+        error = new ErrorResponse(message, 404)
+    }
+
+    //Mongoose duplicate key
+    if (err.code === 1100){
+        const message = 'Duplicate field entered'
+        error = new ErrorResponse(message, 400)
+    }
+
+    //Mongoose validation
+    if (err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new ErrorResponse(message, 400)
+    }
+
+    res.status(error.statusCode || 500).json({success: false, error: error.message || 'Server Error'})
 }
 
 module.exports = errorHandler;
