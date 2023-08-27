059e17d23dc99dff041f7c321364e259cddac366
Akhmadillo Mamirov <105142060+akhmadmamirov@users.noreply.github.com>
2023-08-27 14:51:52 -0400

    Update geocoder.js
    Removing path from env variable

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config/config.env" });
+// dotenv.config({ path: "./config/config.env" });
 const NodeGeocoder = require('node-geocoder')
 
 const options = {
